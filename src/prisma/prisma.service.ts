c733dba885e7e12c1c8df8870d7852796b73a941
PRATANGGA <pratanggajulang31@gmail.com>
2025-04-27 18:21:24 +0700

    Initial Commit

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
+import { env } from 'process';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
